7ba975978aeaf427692c1a064ff57e67215f9239
gopal <gdgk2015@gmail.com>
2023-12-27 20:04:20 -0600

    Day-1.Hotel booking code done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1.register account");
+        System.out.println("2.book reservation");
+        System.out.println("3.general bill");
+        System.out.println("4.feedback");
     }
 }
